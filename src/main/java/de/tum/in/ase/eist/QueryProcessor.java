1419c245ae3cf88b239fd33e3895c69fd070701f
Dennis <dp.jandow@gmail.com>
2022-07-14 15:25:18 +0200

    sth doesnt work

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -9,27 +9,27 @@ import java.util.Comparator;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
-        System.out.println(query);
-        if (query.contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-            return "MyTeam";
-        } else if (query.contains("what is")) { // TODO extend the programm here
-            String[] arr = query.split(" ");
-            return String.valueOf(
-                    Integer.parseInt(arr[arr.length - 3].replaceAll("[^\\d.]", "").strip())
-                            + Integer.parseInt(arr[arr.length - 1].replaceAll("[^\\d.]", "").strip())
-            );
-        } else if (query.contains("which of the following")) {
-            String[] arr = query.split("largest:");
-            return Arrays.stream(arr[1].split(","))
-                    .map(i -> Integer.parseInt(i.replaceAll("[^\\d.]", "").strip()))
-                    .max(Integer::compareTo).toString();
-        } else {
-            return "";
-        }
+        query = query.toLowerCase();
+        return query;
+//        if (query.contains("shakespeare")) {
+//            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+//                    "English poet, playwright, and actor, widely regarded as the greatest " +
+//                    "writer in the English language and the world's pre-eminent dramatist.";
+//        } else if (query.contains("name")) {
+//            return "MyTeam";
+//        } else if (query.contains("what is")) { // TODO extend the programm here
+//            String[] arr = query.split(" ");
+//            return String.valueOf(
+//                    Integer.parseInt(arr[arr.length - 3].replaceAll("[^\\d.]", "").strip())
+//                            + Integer.parseInt(arr[arr.length - 1].replaceAll("[^\\d.]", "").strip())
+//            );
+//        } else if (query.contains("which of the following")) {
+//            String[] arr = query.split("largest:");
+//            return Arrays.stream(arr[1].split(","))
+//                    .map(i -> Integer.parseInt(i.replaceAll("[^\\d.]", "").strip()))
+//                    .max(Integer::compareTo).toString();
+//        } else {
+//            return "";
+//        }
     }
 }
