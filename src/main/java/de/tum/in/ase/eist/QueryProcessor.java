9273c134ef31eebc3bab5caffc35e07f81155b9e
Dennis <dp.jandow@gmail.com>
2022-07-14 15:29:38 +0200

    with lösung

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -16,25 +16,28 @@ public class QueryProcessor {
     public String process(String query) {
         query = query.toLowerCase();
         logger.info(query);
+        String res;
         if (query.contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+            res = "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-            return "MyTeam";
+            res = "MyTeam";
         } else if (query.contains("what is")) { // TODO extend the programm here
             String[] arr = query.split(" ");
-            return String.valueOf(
+            res = String.valueOf(
                     Integer.parseInt(arr[arr.length - 3].replaceAll("[^\\d.]", "").strip())
                             + Integer.parseInt(arr[arr.length - 1].replaceAll("[^\\d.]", "").strip())
             );
         } else if (query.contains("which of the following")) {
             String[] arr = query.split("largest:");
-            return Arrays.stream(arr[1].split(","))
+            res = Arrays.stream(arr[1].split(","))
                     .map(i -> Integer.parseInt(i.replaceAll("[^\\d.]", "").strip()))
                     .max(Integer::compareTo).get().toString();
         } else {
-            return "";
+            res = "";
         }
+        logger.info("Ergebnis " + res);
+        return res;
     }
 }
