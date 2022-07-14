c32a8165aeba99604a51e6f23bf6eba7fead197d
Dennis <dp.jandow@gmail.com>
2022-07-14 14:38:37 +0200

    log?

--- src/main/java/de/tum/in/ase/eist/AnswerResource.java
+++ src/main/java/de/tum/in/ase/eist/AnswerResource.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnswerResource {
 
 	private final Logger logger = LoggerFactory.getLogger(AnswerResource.class);
-
+ 
 	private final QueryProcessor queryProcessor;
 
 	public AnswerResource(QueryProcessor queryProcessor) {
